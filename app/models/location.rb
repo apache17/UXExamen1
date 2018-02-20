37ebef9c8eada40b11ebe1276567a444ecccc834
Boris Garcia <brgp@live.com>
2018-02-19 23:21:29 -0600

    Recording_views

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,2 +1,6 @@
 class Location < ApplicationRecord
+    belongs_to :user, optional: true
+	has_many :recording, dependent: :destroy
+
+	validates :name, presence: true, length: { minimum: 5 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-    get 'location/index'
-    resources :location
+    resources :locations do
+  		resources :recordings
+  	end
     root 'location#index'
 end
