a4a734b97b226857592ca0d80664192d7193914b
Boris Garcia <brgp@live.com>
2018-02-19 22:56:13 -0600

    views

--- app/controllers/location_controller.rb
+++ app/controllers/location_controller.rb
@@ -1,2 +1,48 @@
 class LocationController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  def index
+  	@location = Location.all.order('Created_at DESC')
+  end
+
+  def new
+    @location = Location.new
+  end
+
+  def edit
+    @location = Location.find(location[:id])
+  end
+
+  def create
+  	@location = current_user.Location.new(location_params)
+
+  	@location.save
+  		redirect_to @location
+  end
+
+  def show
+  	@location = Location.find(location[:id])
+  end
+
+  def update
+    @location = Location.find(location[:id])
+
+    if @location.update(location_params)
+      redirect_to @location
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @location = Location.find(location[:id])
+    @location.destroy
+    
+    redirect_to location_path
+end
+  
+  
+  private
+  def location_params
+    location.require(:location).permit(:name)
+  end
 end
--- app/controllers/recording_controller.rb
+++ app/controllers/recording_controller.rb
@@ -1,2 +1,27 @@
 class RecordingController < ApplicationController
+    	before_action :authenticate_user!
+	def create
+		@location = Location.find(params[:location_id])
+		@recording = @location.comments.create(recording_param)
+		@recording.user_id = current_user.id if current_user
+		@recording.save
+
+		redirect_to location_path(@location)
+	end
+
+	def update
+		@location = Location.find(params[:id])
+
+    	if @location.update(location_params)
+      		redirect_to @recording
+    	else
+      		render 'edit'
+    	end
+  	end
+
+
+	private
+	def recording_param
+		params.require(:comment).permit(:temp, :status, :user_id)
+	end
 end
